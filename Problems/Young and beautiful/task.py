c7f929584d8dc1c56e058f2ac746d4dfdf27f481
Zh00c3q <maciej.a.zak@gmail.com>
2020-07-04 23:00:51 +0200

    initial commit

---
+++ Problems/Young and beautiful/task.py
@@ -0,0 +1,5 @@
+jack_age = int(input())
+alex_age = int(input())
+lana_age = int(input())
+
+print(min(int(jack_age), int(alex_age), int(lana_age)))
---
+++ Simple Chatty Bot/task/bot/bot — kopia.py
@@ -0,0 +1,27 @@
+# write your code here
+bot_name = "Zhab00n"
+birth_year = 2020
+print("Hello! My name is {0}.".format(bot_name))
+print("I was created in {0}.".format(birth_year))
+print("Please, remind me your name")
+
+user_name = input()
+print("What a great name you have, {0}.".format(user_name))
+print("Let me guess your age")
+
+remainder3 = int(input)
+remainder5 = int(input)
+remainder7 = int(input)
+
+user_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+print("Your age is (0): that's a good time to start programming!".format(user_age))
+print("Now I will prove to you that I can count to any number you want.")
+
+number = int(input())
+i=0
+while i <= number:
+    print(str(i) + " !")
+    i += 1
+
+print("Let's test your programming knowledge.")
